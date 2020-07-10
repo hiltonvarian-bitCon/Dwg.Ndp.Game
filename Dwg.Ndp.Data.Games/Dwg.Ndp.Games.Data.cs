a43b301741f3f292d40e5616d0fe1c2432a5919e
Hilton James Varian <varianhilton@gmail.com>
2020-07-10 20:38:05 +0200

    ID Author Date Message 87f19d81baa6b045d522024ea31d83e7643956d6 Hilton James Varian <varianhilton@gmail.com> 7/10/2020 4:02:50 PM +00:00 Add project file

---
+++ Dwg.Ndp.Data.Games/Dwg.Ndp.Games.Data.cs
@@ -0,0 +1,70 @@
+﻿namespace Dwg.Ndp.Data.Games
+{
+    public  enum NatureElements:int
+    {
+    EarthElem=0,AirElem=1,FireElem=2,
+    WaterElem =3,DarcElem=4,NoElem=-1
+    }
+
+    public  enum NatElementsGF:int
+    {
+    EarthElemGF=0,AirElemGF=1,FireElemGF=2,
+    WaterGF =3,DarcElemGF=4,NoElemGF=-1
+    }
+    public class TDwgNdpGamesData
+    {
+
+    private static NatureElements elements = NatureElements.NoElem;
+    private static NatElementsGF elementsGF = NatElementsGF.NoElemGF;
+    public TDwgNdpGamesData():this(elements)
+    {
+    //Default Constructor
+    }
+    public void InitAllData()
+    {
+    
+    }
+    private string[] thelemArr = { "Earth Elemental", "Air Elemental", "Fire Elemental", "Water Elementa", "Darc Elemnta" };
+
+    private ref NatureElements GetNameElements(string[]value)
+    {
+    thelemArr = value;
+
+    return ref elements;
+    }
+
+    public TDwgNdpGamesData(NatureElements natureElements)
+    {
+   
+    }
+    public class TDwgGameDats:TDwgNdpGamesData
+    {
+    public TDwgGameDats():this(elements)
+    {
+    //Default Value
+    }
+
+    public NatureElements TheNatElements
+    {
+  get => elements;
+  set => elements = value; 
+    }
+
+    public string[] ElementalArr
+    {
+  get => thelemArr;
+  set => thelemArr = value;
+    }
+
+    public NatElementsGF TheElementsGF
+    {
+  get => elementsGF;
+  set => elementsGF = value;
+    }
+    public TDwgGameDats(NatureElements theNatureElements)
+    {
+    elements = theNatureElements;
+    }
+    }
+    }
+    }
