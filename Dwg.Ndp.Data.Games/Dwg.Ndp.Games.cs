b9c431d29b36428c5af38ea167dce5a3201da013
Hilton James Varian <varianhilton@gmail.com>
2020-07-10 18:55:50 +0200

    ID Author Date Message 87f19d81baa6b045d522024ea31d83e7643956d6 Hilton James Varian <varianhilton@gmail.com> 7/10/2020 4:02:50 PM +00:00 Add project file

--- Dwg.Ndp.Data.Games/Dwg.Ndp.Games.cs
+++ Dwg.Ndp.Data.Games/Dwg.Ndp.Games.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
 
-namespace Dwg.Ndp.Data.Games
-{
+    namespace Dwg.Ndp.Data.Games
+    {
     public class TDwgNdpGamesData
     {
     }
-}
+    }
