21601e4bbf96cdbf97a4d80ad6f1e63017719f67
Hilton James Varian <varianhilton@gmail.com>
2020-07-11 10:32:44 +0200

    ID Author Date Message 87f19d81baa6b045d522024ea31d83e7643956d6 Hilton James Varian <varianhilton@gmail.com> 7/10/2020 4:02:50 PM +00:00 Add project file

--- Dwg.Ndp.Data.Games/Dwg.Ndp.Games.Data.cs
+++ Dwg.Ndp.Data.Games/Dwg.Ndp.Games.Data.cs
@@ -33,10 +33,20 @@
     }
     protected void SetAllDataValues()
     {
-    TheNatElemDataSet = NatureElementsFlags.AirElem;
-   
+
+    TheFlags = NatureElementsFlags.AirElem;
+    NatElementsFlagsSetGF = NatElementsFlagsGF.AirElemGF;
     }
     private string[] thelemArr = { "Earth Elemental", "Air Elemental", "Fire Elemental", "Water Elemental", "Darc Elemntal" };
+
+
+    public  void SetAllFlags(TDwgGameDats theval, NatureElementsFlags nature, NatElementsFlagsGF flagsGF ,in  NatureElementsFlags allTheFlags)
+    {
+    theval.TheFlags         = nature;
+    theval.TheElementsGF    = flagsGF;
+
+    TheFlags = GetNameElements(theval);
+    }
 
     private ref NatureElementsFlags GetNameElements(string[]value)
     {
@@ -44,13 +54,14 @@
 
     return ref elements;
     }
-    public virtual  NatureElementsFlags GetNameElements(TDwgGameDats theData)
+
+    protected  virtual  NatureElementsFlags GetNameElements(TDwgGameDats theData)
     {
     if (theData == null)
     {
     throw new ArgumentNullException(nameof(theData));
     }
-    switch (theData.TheNatElemDataSet)
+    switch (theData.TheFlags)
     {
     case NatureElementsFlags.EarthElem:
          elements = NatureElementsFlags.EarthElem;
@@ -73,17 +84,53 @@
     }
     return theData.TheNatElementsFlags;
     }
-    public NatureElementsFlags TheNatElemDataSet
+    protected virtual  NatElementsFlagsGF  GetElementsFlagsGF(TDwgGameDats gameDats)
+    {
+    if (gameDats==null)
+    {
+    throw new ArgumentException(nameof(gameDats));
+    }
+    switch (gameDats.NatElementsFlagsSetGF)
+    {
+    case NatElementsFlagsGF.EarthElemGF:
+         elementsGF = NatElementsFlagsGF.EarthElemGF;
+    break;
+    case NatElementsFlagsGF.AirElemGF:
+         elementsGF = NatElementsFlagsGF.AirElemGF;
+    break;
+    case NatElementsFlagsGF.FireElemGF:
+         elementsGF = NatElementsFlagsGF.FireElemGF;
+    break;
+    case NatElementsFlagsGF.WaterGF:
+         elementsGF = NatElementsFlagsGF.WaterGF;
+    break;
+    case NatElementsFlagsGF.DarcElemGF:
+         elementsGF = NatElementsFlagsGF.DarcElemGF; 
+    break;
+   default:
+    gameDats.NatElementsFlagsSetGF = NatElementsFlagsGF.NoElemGF;
+   break; 
+    }
+    return gameDats.TheElementsGF;
+    }
+   
+    public NatureElementsFlags TheFlags
     {
   get => elements;
   set => elements = value;
+    }
+
+    public NatElementsFlagsGF NatElementsFlagsSetGF
+    {
+  get => elementsGF;
+  set => elementsGF = value;
     }
     public TDwgNdpGamesData(NatureElementsFlags natureElements)
     {
    
     }
-    public class TDwgGameDats:TDwgNdpGamesData
-    {
+    public class TDwgGameDats: TDwgNdpGamesData
+        {
     public TDwgGameDats():this(elements)
     {
     //Default Value
@@ -101,7 +148,7 @@
   set => thelemArr = value;
     }
 
-    public NatElementsFlagsGF TheElementsGF
+    internal NatElementsFlagsGF TheElementsGF
     {
   get => elementsGF;
   set => elementsGF = value;
@@ -111,10 +158,14 @@
     elements = theNatureElements;
     }
 
-    public override NatureElementsFlags GetNameElements(TDwgGameDats theData)
+    protected override NatureElementsFlags GetNameElements(TDwgGameDats theData)
     {
 
     return base.GetNameElements(theData);
+    }
+    protected override NatElementsFlagsGF GetElementsFlagsGF(TDwgGameDats gameDats)
+    {
+    return base.GetElementsFlagsGF(gameDats);
     }
     }
     }
---
+++ Dwg.Ndp.Person/Dwg.Ndp.Person.Char.cs
@@ -0,0 +1,9 @@
+﻿    namespace Dwg.Ndp.Person
+    {
+    using Dwg.Ndp.Data.Games;
+    public interface IDwgNdpPerson
+    {
+    NatureElementsFlags InitAllPersons(NatureElementsFlags nature,NatElementsFlagsGF elementsFlagsGF);
+    NatureElementsFlags TheCharPerson { get; }
+    }
+    }
