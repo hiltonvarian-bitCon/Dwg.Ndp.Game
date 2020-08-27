50dccd22cf341d9fd46f0605297717e4caef2c36
Hilton James Varian <varianhilton@gmail.com>
2020-08-27 19:48:14 +0200

    ID Author Date Message  Hilton James Varian <varianhilton@gmail.com> 8/18/2020 9:14:14 AM +00:00 ID Author Date

--- Dwg.Client.Game.Browser/Dwg.ClientGame.Brows.cs
+++ Dwg.Client.Game.Browser/Dwg.ClientGame.Brows.cs
@@ -22,18 +22,18 @@ namespace Dwg.Client.Game.Browser
             this.TheGameWebBrowser.Location = new System.Drawing.Point(0, 0);
             this.TheGameWebBrowser.MinimumSize = new System.Drawing.Size(20, 20);
             this.TheGameWebBrowser.Name = "TheGameWebBrowser";
-            this.TheGameWebBrowser.Size = new System.Drawing.Size(742, 701);
+            this.TheGameWebBrowser.Size = new System.Drawing.Size(713, 701);
             this.TheGameWebBrowser.TabIndex = 0;
             // 
             // TDwgClientGameBrowser
             // 
-            this.ClientSize = new System.Drawing.Size(742, 701);
+            this.ClientSize = new System.Drawing.Size(713, 701);
             this.Controls.Add(this.TheGameWebBrowser);
             this.Name = "TDwgClientGameBrowser";
             this.Tag = "The_Web_Browse";
             this.Load += new System.EventHandler(this.OnGameBrowserLoad);
             this.ResumeLayout(false);
-        
+
         }
         public TDwgClientGameBrowser(object browsweb)
         {
